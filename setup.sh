26c7becb8c474fe56794fe2ec936ecab0fe86d20
Rp7rf <westhouse51@gmail.com>
2018-08-14 23:11:49 +0900

    add vim template

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ ln -sf ~/dotfiles/bash/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
 # tmux copy command
-brew install reattach-to-user-namespace
+if [[ v1=`brew list  | grep reattach-to-user-namespace` -ne 'reattach-to-user-namespace' ]]; then
+  brew install reattach-to-user-namespace
+fi
---
+++ vim/.vimrc
@@ -0,0 +1,8 @@
+set expandtab
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
+set autoindent
+set smartindent
+
+autocmd BufNewFile *.cpp 0r ~/dotfiles/templates/cpp.txt
